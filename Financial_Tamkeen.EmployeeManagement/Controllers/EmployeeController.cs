2f1dbd572146f1d80862c84a2cf1897b77441b44
AmProj2024 <144052537+AmProj2024@users.noreply.github.com>
2024-02-13 05:28:16 +0300

    Update EmployeeController.cs

--- Financial_Tamkeen.EmployeeManagement/Controllers/EmployeeController.cs
+++ Financial_Tamkeen.EmployeeManagement/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
