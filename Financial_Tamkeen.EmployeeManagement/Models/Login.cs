46900cef35f5fcad28f5b71f23e5eda0cc3d0002
AmProj2024 <144052537+AmProj2024@users.noreply.github.com>
2024-02-13 05:04:51 +0300

    Create Login.cs

---
+++ Financial_Tamkeen.EmployeeManagement/Models/Login.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Financial_Tamkeen.EmployeeManagement.Models
+{
+Public class login 
+{
+
+}
+
+
+
+}
+
+
