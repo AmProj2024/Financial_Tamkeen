f4d85037b350674eec68f5dae28b9ba7c14785a9
AmProj2024 <144052537+AmProj2024@users.noreply.github.com>
2024-02-13 05:17:33 +0300

    Update LoginController.cs

--- Financial_Tamkeen.EmployeeManagement/Controllers/LoginController.cs
+++ Financial_Tamkeen.EmployeeManagement/Controllers/LoginController.cs
@@ -1,1 +1,124 @@
 
+using Financial_Tamkeen.EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Financial_Tamkeen.EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController
+    {
+
+        private readonly AppDbContex _DbContext;
+        private readonly JsonSerializerOptions _jsonOptions;
+
+        public EmployeeController(AppDbContex DbContext)
+        {
+            this._DbContext = DbContext;
+            _jsonOptions = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve,
+                IgnoreNullValues = true,
+                MaxDepth = 10  // Specify the maximum depth to avoid excessive nesting
+            };
+
+        }
+        // GET: api/<EmployeeController>
+        [HttpGet("GetAllEmployee")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var employees = await _DbContext.Employee
+            .ToListAsync();
+
+            var serializedEmployees = System.Text.Json.JsonSerializer.Serialize(employees, _jsonOptions);
+
+            return Content(serializedEmployees, "application/json");
+
+            //    return Ok(employees);
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{GetEmployeeId}")]
+        public async Task<ActionResult<Employee>>GetEmployeeId(int id)
+        {
+
+            var employee = await _DbContext.Employee
+                .FirstOrDefaultAsync(e => e.EmployeeId == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var serializedEmployees = System.Text.Json.JsonSerializer.Serialize(employee, _jsonOptions);
+
+            return Content(serializedEmployees, "application/json");
+        }
+
+
+
+        // POST api/<EmployeeController>
+        [HttpPost("CreateNEwEmployee")]
+        
+        public async Task<ActionResult<Employee>> Post(Employee Emp)
+        {
+            var employee =await this._DbContext.Employee.AddAsync(Emp);
+            await this._DbContext.SaveChangesAsync();   
+            return  Ok(employee);
+
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            _DbContext.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _DbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_DbContext.Employee.Any(e => e.EmployeeId == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{DeleteEmployeeid}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _DbContext.Employee.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _DbContext.Employee.Remove(employee);
+            await _DbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
